667257af48d8092e4f9b38a78e92654b77327a68
jgelsana <jgelsana@77global.biz>
2024-03-14 02:57:39 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogItemComponent } from './blog/components/blog-item/blog-item.component';
+import { BookItemComponent } from './book/components/book-item/book-item.component';
+import { FormComponent } from './user/components/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'user',
+    pathMatch: 'prefix'
+  },
+  {
+    path: 'user',
+    component: FormComponent
+  },
+  {
+    path: 'blog',
+    component: BlogItemComponent
+  },
+  {
+    path: 'book',
+    component: BookItemComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
